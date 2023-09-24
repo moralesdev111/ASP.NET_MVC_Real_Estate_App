febab1f0c6768fa0710fc5116f308685386c841a
Guillermo Mora <moralesdev111@gmail.com>
2023-09-24 21:56:07 +0200

    Up to CRUD

--- Models/Listings.cs
+++ Models/Listings.cs
@@ -1,14 +1,21 @@
-﻿namespace RealEstateWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealEstateWebsite.Models
 {
     public class Listings
     {
         public int Id { get; set; }
+        [Display (Name="House")]
         public string? houseName { get; set; }
+        [Display (Name ="Cost")]
+        [Column(TypeName ="double(15)")]
         public int houseCost { get; set; }
+        [Display (Name ="Address")]
         public string? address { get; set; }
-
+        [Display (Name ="Squared Metres")]
         public double squaredMetres { get; set; }
-
+        [Display (Name ="City Location")]
         public string? city { get; set; }
 
     }
