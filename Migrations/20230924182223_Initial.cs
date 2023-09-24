180f7a5157788df9e8947de47102c0507a2bbaa8
Guillermo Mora <moralesdev111@gmail.com>
2023-09-24 20:54:42 +0200

    Starting Entity Framework Connection

---
+++ Migrations/20230924182223_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RealEstateWebsite.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Listings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    houseName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    houseCost = table.Column<int>(type: "int", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    squaredMetres = table.Column<double>(type: "float", nullable: false),
+                    city = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Listings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Listings");
+        }
+    }
+}
