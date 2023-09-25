3b495a6b078d9a9afc9e6ef139316606f811e660
Guillermo Mora <moralesdev111@gmail.com>
2023-09-25 23:16:53 +0200

    finishing touches

--- Models/Listings.cs
+++ Models/Listings.cs
@@ -6,19 +6,32 @@ namespace RealEstateWebsite.Models
     public class Listings
     {
         public int Id { get; set; }
+
+        [StringLength(65,MinimumLength=3)]
+        [Required]
         [Display (Name="House")]
         public string? houseName { get; set; }
+
         [Display (Name ="Cost (€)")]
-        [Column(TypeName ="double(15)")]
+        [Range(1,1000000)]
+        [DataType(DataType.Currency)]
         public int houseCost { get; set; }
+
         [Display (Name ="Address")]
+        [Required]
         public string? address { get; set; }
+
+        [Range (0,99)]
+        [Required]
         [Display (Name ="Squared Metres")]
         public double squaredMetres { get; set; }
+
+        [Required]
         [Display (Name ="City Location")]
         public string? city { get; set; }
 
         [Display (Name ="Balcony")]
+        [StringLength(5,MinimumLength =1)]
         public string? hasBalcony { get; set; }
 
     }
